10004a141bfda68dc7573dc13525c78ce8699ada
V1ctor W1ll1an <victorwillian1012@gmail.com>
2022-01-20 21:00:24 -0300

    lidando com static files

---
+++ mysite/pages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request=request, template_name='pages/index.html')
+def about(request):
+    return render(request=request, template_name='pages/about.html')
